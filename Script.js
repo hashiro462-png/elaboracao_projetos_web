10006debf935f2569a4c208d95d546d897324b33
paulo henrique <hashiro462@gmail.com>
2025-08-16 10:57:14 -0300

    Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+// Espera a página carregar
+document.addEventListener("DOMContentLoaded", function() {
+    const botao = document.getElementById("meuBotao");
+
+    botao.addEventListener("click", function() {
+        alert("Você clicou no botão! 🎉");
+    });
+});
